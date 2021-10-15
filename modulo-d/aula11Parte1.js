43e22d1528d3c14176f31ac0ee1ab6de8b1145fb
Josue Victor Arroxellas <josue.arroxellas@unimedmaceio.local>
2021-10-15 12:49:59 -0300

    aula 11

---
+++ modulo-d/aula11Parte1.js
@@ -0,0 +1,5 @@
+var vel = 12
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60){
+    console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
+}console.log(`Dirija sempre usando cinto de segurança`)
