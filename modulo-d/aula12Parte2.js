d50a02ac4331a2d97e9d74082a781737f329bb5c
josuevictor <victorarroxellas01@gmail.com>
2021-10-19 18:29:04 -0300

    aula 12 parte

---
+++ modulo-d/aula12Parte2.js
@@ -0,0 +1,10 @@
+var idade = 66
+if (idade < 16){
+    console.log('Nao vota')
+}else if (idade < 18){
+        console.log('Voto opcional')
+}else if (idade >= 18 && idade <= 65){
+   console.log('Voto obrigatório')
+}else{
+    console.log('Voto opcional')
+}
