3b039d0a98dcfe3a05363215d0efb5459c5ba82b
josuevictor <victorarroxellas01@gmail.com>
2021-10-19 17:05:22 -0300

    ex011

--- modulo-d/aula11Parte1.js
+++ modulo-d/aula11Parte1.js
@@ -1,5 +1,6 @@
-var vel = 12
+var vel = 61
 console.log(`A velocidade do seu carro é de ${vel}km/h`)
 if (vel > 60){
     console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
-}console.log(`Dirija sempre usando cinto de segurança`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
