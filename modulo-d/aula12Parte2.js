591b238010ecb26d0973efbf3c6fd7a3ba086cbc
josuevictor <victorarroxellas01@gmail.com>
2021-10-19 23:17:46 -0300

    ex012

--- modulo-d/aula12Parte2.js
+++ modulo-d/aula12Parte2.js
@@ -1,10 +1,8 @@
-var idade = 66
+var idade = 17
 if (idade < 16){
     console.log('Nao vota')
-}else if (idade < 18){
+}else if (idade < 18 || idade > 65){
         console.log('Voto opcional')
-}else if (idade >= 18 && idade <= 65){
-   console.log('Voto obrigatório')
 }else{
-    console.log('Voto opcional')
+   console.log('Voto obrigatório')
 }
