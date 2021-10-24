cc508a7002d6251a0c39816ff53e9298d772d656
josuevictor-notebook <victorarroxellas01@gmail.com>
2021-10-24 09:44:18 -0300

    modulo f - vetores

--- modulo-f/vetor.js
+++ modulo-f/vetor.js
@@ -1,1 +1,10 @@
-console.log('teste')
+var vet = []
+var insere = 0
+while (insere <= 20){
+    vet.push(insere)
+    insere++
+}
+console.log(vet)
+console.log(`Tmanho do vetor: ${vet.length}`)
+let pos = console.log(vet.indexOf(12))
+console.log(``)
