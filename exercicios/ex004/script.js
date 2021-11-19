487e254a52f63a315f3f7ba031ef3a74533d86cb
Desktop <victorarroxellas224@gmail.com>
2021-11-18 22:26:04 -0300

    ex004

---
+++ exercicios/ex004/script.js
@@ -0,0 +1,20 @@
+function contar(){
+    var inicio = document.querySelector('#inicio');
+    var fim = document.querySelector('#fim');
+    var passos = document.querySelector('#passos');
+    var res = document.querySelector('res')
+    var ini = inicio.value;
+    var fim = fim.value;
+    var qtdPassos = passos.value;
+    console.log('INÌCIO: ', ini);
+    console.log('FIM:   ', fim);
+    //console.log('PASSOS: ', qtdPassos);
+    
+    for(cont=ini; cont <= fim ; cont++){
+        console.log('-> ' + cont);
+    }
+    //console.log('cont', cont)
+    //console.log('fim',fim)
+    res.innerHTML = 'teste'
+
+}
